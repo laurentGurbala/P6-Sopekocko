6020fda085a8dc97f71be0964c110f5c2be54c43
laurentGurbala <laurent.gurbala@gmail.com>
2021-02-08 15:27:52 +0100

    Suppression d'une sauce avec gestion d'image

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -1,4 +1,5 @@
 const Sauce = require("../models/sauce");
+const fs = require("fs");
 
 exports.createSauce = (req, res, next) => {
     const sauceObject = JSON.parse(req.body.sauce);
@@ -19,16 +20,23 @@ exports.modifySauce = (req, res, next) => {
     console.log(sauceObject);
 
     // TODO : SUPPRIMER L'IMAGE PRECEDENTE DU DOSSIER
-    
+
     Sauce.updateOne({_id: req.params.id}, {...sauceObject, _id: req.params.id })
     .then(() => res.status(200).json({ message: "Sauce modifiée !"}))
     .catch(error => res.status(400).json({ error }));
 };
 
 exports.deleteSauce = (req, res, next) => {
-    Sauce.deleteOne({ _id: req.params.id})
-    .then(() => res.status(200).json({ message: "Sauce supprimée !"}))
-    .catch(error => res.status(400).json({ error }));
+    Sauce.findOne({_id: req.params.id})
+    .then(sauce => {
+        const filename = sauce.imageUrl.split("/images/")[1];
+        fs.unlink(`images/${filename}`, ()=> {
+            Sauce.deleteOne({ _id: req.params.id})
+            .then(() => res.status(200).json({ message: "Sauce supprimée !"}))
+            .catch(error => res.status(400).json({ error }));
+        });
+    })
+    .catch(error => res.status(400).json( { error }));
 };
 
 exports.getOneSauce = (req, res, next) => {
