5dd190c0b7d011cbf923d60fb3ee5b6725807542
laurentGurbala <laurent.gurbala@gmail.com>
2021-02-24 11:16:07 +0100

    Bloque nombre de tentative

--- routes/user.js
+++ routes/user.js
@@ -2,10 +2,17 @@ const express = require("express");
 const router = express.Router();
 
 const userCtrl = require("../controllers/user");
+const rateLimit = require("express-rate-limit");
 
-// Todo : nombre d'essaie login
+const numberTry = rateLimit({
+    windowMs: 0.5 * 60000,  // 30 sec
+    max: 5,                 // nombre de tentative
+    message:
+    "Trop de tentative de connexion... Veuillez réessayer un peu plus tard, merci."
+});
+
 
 router.post("/signup", userCtrl.signup);
-router.post("/login", userCtrl.login);
+router.post("/login", numberTry, userCtrl.login);
 
 module.exports = router;
